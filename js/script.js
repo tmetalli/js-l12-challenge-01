ed189d9d0005d57d75b2231a18fe0ce0a749c814
T Metalli <t6metalli@yahoo.com>
2022-11-28 16:40:18 +0000

    added rest of function and event listener to button

--- js/script.js
+++ js/script.js
@@ -3,10 +3,29 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const GetImage = async function () {
+const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
 
-GetImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImages(randomImage);
+};
+
+const displayImages = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+
+};
+
+button.addEventListener("click", function () {
+    getImage();
+
+});

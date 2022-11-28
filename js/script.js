e9c18842b054c2dfc2dbfa0791241bdd41f33156
T Metalli <t6metalli@yahoo.com>
2022-11-28 15:48:08 +0000

    added Lorem Picsum API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const GetImage = async function () {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    console.log(images);
+};
+
+GetImage();
